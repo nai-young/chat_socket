2c3f5c1495884738b1a8374a2c140a84ff6f41c7
N Young <60322086+nai-young@users.noreply.github.com>
2020-08-16 18:31:14 +0100

    Front end app

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import config from './config'
+import io from 'socket.io-client'
+import Paper from '@material-ui/core/Paper'
+import Typography from '@material-ui/core/Typography'
+
+import BottomBar from './BottomBar'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor (props) {
+    super(props)
 
-export default App;
+    this.state = {
+      name: '',
+      content: '',
+      chat: []
+    }
+  }
+  componentDidMount() {
+    this.socket = io(config[process.env.NODE_ENV].endpoint)
+    this.socket.on('init', (msg) => {
+      let msgRev = msg.reverse()
+      this.setState(state => ({
+        chat: [...state.chat, ...msgRev],
+      }), this.scrollToBottom)
+    })
+    this.socket.on('push', (msg) => {
+      this.setState((state) => ({
+        chat: [...state.chat, msg],
+      }), this.scrollToBottom);
+    })
+  }
+  handleName(e){
+    this.setState({
+      name: e.target.value
+    })
+  }
+  handleContent(e) {
+    this.setState({
+      content: e.target.value
+    })
+  }
+  handleSubmit(e) {
+    e.preventDefault()
+
+    this.socket.emit('message', {
+      name: this.state.name,
+      content: this.state.content
+    })
+    this.setState(state => {
+      return {
+        chat: [...state.chat, {
+          name: state.name,
+          content: state.content
+        }],
+        content: '',
+      }
+    }, this.scrollToBottom)
+  }
+  scrollToBottom() {
+    const chat = document.getElementById('chat')
+    chat.scrollTop = chat.scrollHeight
+  }
+  render () {
+    return (
+      <div className="App">
+        <Paper id="chat" elevation={3}>
+          {this.state.chat.map((element, i) => {
+            return (
+              <div key={i}>
+                <Typography variant="caption" className="name">
+                  {element.name}
+                </Typography>
+                <Typography variant="body1" className="content">
+                  {element.content}
+                </Typography>
+              </div>
+            )
+          })}
+        </Paper>
+        <BottomBar
+          content={this.state.content}
+          handleContent={this.handleContent.bind(this)}
+          handleName={this.handleName.bind(this)}
+          handleSubmit={this.handleSubmit.bind(this)}
+          name={this.state.name}
+        />
+      </div>
+    )
+  }
+}
+export default App
